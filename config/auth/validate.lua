f9176be79e8a474cf88bcf7568cae9b7556cb03a
Andrée Hansson <peolanha@gmail.com>
2017-05-15 15:41:01 +0200

    Initial auth implementation

---
+++ config/auth/validate.lua
@@ -0,0 +1,69 @@
+local COOKIE_NAME = "customanalytics"
+
+-- retrieves a ref to a redis connection
+local function getdb()
+  ngx.log(ngx.DEBUG, "auth: Creating redis connection")
+  local redis = require "resty.redis"
+  local red = redis:new()
+
+  red:set_timeout(1000)
+
+  -- redis hostname here:
+  local ok, err = red:connect("redis", 6379)
+  if not ok then
+    return
+  end
+
+  return red
+end
+
+-- retrieves the jwt belonging to a session id
+local function idToJWT(id)
+  if not id then
+    ngx.log(ngx.DEBUG, "auth: No cookie ID found")
+    -- we require a session id to operate
+    return
+  end
+
+  local red = getdb()
+
+  if not red then
+    ngx.log(ngx.DEBUG, "auth: Failed redis connection")
+    -- we cannot continue without a redis connection
+    return
+  end
+
+  local jwt, err = red:get(id)
+
+  if not jwt or jwt == ngx.null then
+    ngx.log(ngx.DEBUG, "auth: No JWT found")
+    -- there was no jwt stored under that session id
+    return
+  end
+
+  -- put redis connection into connection pool for reuse
+  -- 100 max clients, 10 seconds max idle
+  local ok, err = red:set_keepalive(10000, 100)
+  if not ok then
+    ngx.log(ngx.DEBUG, "auth: Failed to keep-alive redis connection")
+    return
+  end
+
+  return jwt
+end
+
+-- validate a request, can be used in location nginx entries to require
+-- a user to have a jwt associated to their session
+local jwt = idToJWT(ngx.var["cookie_" .. COOKIE_NAME])
+
+if not jwt then
+  ngx.header["Content-Type"] = "text/html";
+  ngx.status = ngx.HTTP_UNAUTHORIZED
+  ngx.say("You do not have permission to view this resource.")
+  ngx.exit(ngx.HTTP_UNAUTHORIZED)
+end
+
+-- validate JWT
+
+ngx.req.set_header("Authorization", "Bearer " .. jwt)
+--ngx.req.clear_header("Cookie")
