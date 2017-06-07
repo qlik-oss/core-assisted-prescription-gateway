22aeb396a70da143ebc6d12e036c4501214b0879
Andrée Hansson <peolanha@gmail.com>
2017-06-07 13:06:38 +0200

    Add HTTPS (TLS) support (#13)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-envsubst '$KIBANA_HOST $KIBANA_PORT $VISUALIZER_HOST $VISUALIZER_PORT $QIX_SESSION_HOST $QIX_SESSION_PORT $AUTH_HOST $AUTH_PORT' \
+# TODO: Default CERT_FILE/KEY to dummy path to allow non-https dev?
+
+envsubst '\
+$KIBANA_HOST \
+$KIBANA_PORT \
+$VISUALIZER_HOST \
+$VISUALIZER_PORT \
+$QIX_SESSION_HOST \
+$QIX_SESSION_PORT \
+$AUTH_HOST \
+$AUTH_PORT \
+$CERT_FILE \
+$CERT_KEY \
+' \
   < nginx.conf.template \
   > nginx.conf
 ../bin/openresty -g "daemon off;" -c nginx.conf
