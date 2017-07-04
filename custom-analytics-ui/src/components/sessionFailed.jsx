70dad3513f73a398b318d8013d971efb008c0f53
Helene Rignér <hvs@qlik.com>
2017-07-04 07:54:27 +0200

    Adding correct Card style to sessionFailed (#83)

--- custom-analytics-ui/src/components/sessionFailed.jsx
+++ custom-analytics-ui/src/components/sessionFailed.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Card from './card';
+import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 export default function SessionFailed() {
   return (
-    <Card id="session-failed" title="There was a problem connecting to QIX Engine">
-      <div>
-        <p>This can be caused by several things:</p>
+    <Card id="session-failed" style={{ margin: '20px' }}>
+      <CardTitle title="There was a problem connecting to QIX Engine" />
+      <CardText>
+        This can be caused by several things:
 
         <ul>
           <li>You are not logged in</li>
@@ -13,9 +14,9 @@ export default function SessionFailed() {
           <li>No available QIX Engines</li>
         </ul>
 
-        <p>Please try again in a little while, if you are still having trouble give
-        us a ping <a href="https://github.com/qlik-ea/qliktive-custom-analytics/issues">on GitHub.</a></p>
-      </div>
+        Please try again in a little while, if you are still having trouble give
+        us a ping <a href="https://github.com/qlik-ea/qliktive-custom-analytics/issues">on GitHub.</a>
+      </CardText>
     </Card>
   );
 }
