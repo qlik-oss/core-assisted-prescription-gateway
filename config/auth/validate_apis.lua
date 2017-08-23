5cd3b5678e61ed67e9a84c5ce89186f9dc864098
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-08-23 15:37:41 +0200

    fixed so we are sending the jwt from openresty to the session service (#97)

--- config/auth/validate_apis.lua
+++ config/auth/validate_apis.lua
@@ -59,3 +59,16 @@ function getJWTIfLoggedIn()
     return idToJWT(ngx.var["cookie_" .. COOKIE_NAME])
   end
 end
+
+function validate_user()
+  local jwt = getJWTIfLoggedIn()
+
+  if not jwt then
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+  end
+
+  -- validate JWT?
+
+  ngx.req.set_header("Authorization", "Bearer " .. jwt)
+  --ngx.req.clear_header("Cookie")
+end
