2c044a6bbb995c0b0dfe7a1ac2a95cbacc59498d
Henrik Carlioth <carlioth@users.noreply.github.com>
2017-11-23 13:00:59 +0100

    Update to dev. env for custom analytics ui (#122)
    * Update webpack and cleaned the working with customer analytics UI in openresty

--- run-dev.sh
+++ run-dev.sh
@@ -1,38 +1,28 @@
 #!/bin/sh
 
-CONTAINER_ID=$(docker ps | grep openresty | grep -o '^\S\+')
+CONTAINER_ID=$(docker ps -aqf "name=openresty")
+
 
 if [ "$CONTAINER_ID" != "" ]; then
-  echo "Stopping existing openresty service..."
+  printf "Stopping existing openresty service..."
   docker stop $CONTAINER_ID
 fi
 
-# make sure the dist/ folder exists before building:
 mkdir -p custom-analytics-ui/dist
 
-docker-compose up --build &
+docker-compose up --build -d
+cd custom-analytics-ui
+npm install
 
-echo "Waiting for services to start up..."
+printf
+printf
+printf "\x1b[32m Ready to hack!\n"
+printf " Please go to https://localhost/ (mind the ending slash) and hack away...\n"
+printf " Press ctrl-c to cancel dev mode...\x1b[0m\n"
 
-WEBPACK_IS_DOWN="true"
+sleep 5
 
-while [ "$WEBPACK_IS_DOWN" == "true" ]; do
-  WEBPACK_IS_DOWN=$(curl http://localhost:8080 -s -f -o /dev/null || echo "true")
-  sleep 1
-done
-
-echo
-echo
-echo "Ready to hack! Please go to https://localhost/dev/ (mind the ending slash)"
-echo "and hack away..."
-echo
-echo
-echo "Press any key to cancel dev mode..."
-echo
-echo
-
-# wait for keyboard input:
-read -n1 -r -p ""
+npm run start
 
 docker-compose down
 
