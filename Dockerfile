bad7fbb6253593081f0088ff5d1a67d9b2857b97
Henrik Carlioth <carlioth@users.noreply.github.com>
2018-02-13 16:12:41 +0100

    Added admin role for certain paths (#141)
    * Will verify role from jwt, "User" and "Admin" roles for now.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ RUN apk add --no-cache gettext libintl openssl
 
 WORKDIR /usr/local/openresty/nginx
 
+# Download jwt lua package
+RUN \
+ wget -qO- https://github.com/SkyLothar/lua-resty-jwt/releases/download/v0.1.11/lua-resty-jwt-0.1.11.tar.gz | tar xvz -C /tmp && \
+ cp -r /tmp/lua-resty-jwt-0.1.11/lib/resty/* /usr/local/openresty/lualib/resty/ && \
+ rm -rf /tmp/*
+
 COPY ./config/ .
 COPY ./custom-analytics-ui/dist/ ./html/custom-analytics-ui
 COPY ./picasso-sandbox/ ./html/picasso-sandbox
--- config/auth/validate_apis.lua
+++ config/auth/validate_apis.lua
@@ -60,9 +60,7 @@ function getJWTIfLoggedIn()
   end
 end
 
-function validate_user(redirect)
-  local jwt = getJWTIfLoggedIn()
-
+local function does_jwt_exist(jwt)
   if not jwt then
     if redirect then
       ngx.redirect("/login/" .. os.getenv("AUTH_STRATEGY") .. '?redirect_url='.. ngx.var.uri, 302)
@@ -70,9 +68,25 @@ function validate_user(redirect)
       ngx.exit(ngx.HTTP_UNAUTHORIZED)
     end
   end
+  return
+end
 
-  -- validate JWT?
-
+function validate_user(redirect)
+  local jwt = getJWTIfLoggedIn()
+  does_jwt_exist(jwt)
   ngx.req.set_header("Authorization", "Bearer " .. jwt)
-  --ngx.req.clear_header("Cookie")
+end
+
+function is_admin(redirect)
+  local tempjwt = getJWTIfLoggedIn()
+  does_jwt_exist(jwt)
+
+  local jwt = require "resty.jwt"
+  local jwt_obj = jwt:load_jwt(tempjwt)
+  local userRole = jwt_obj.payload.userRole;
+  if userRole ~= "Admin" then
+    ngx.exit(ngx.HTTP_UNAUTHORIZED)
+  end
+
+  ngx.req.set_header("Authorization", "Bearer " .. tempjwt)
 end
