d216fc796945df037c65d986faef66becb97a54d
Henrik Carlioth <hca@qlik.com>
2019-06-05 15:44:22 +0200

    app uploaded

--- src/enigma-config.js
+++ src/enigma-config.js
@@ -1,7 +1,7 @@
 import qixSchema from 'enigma.js/schemas/12.20.0.json';
 
 const ERR_ABORTED = 15;
-const backendAdress = process.env.NODE_ENV === 'production' ? `${process.env.BACKEND}/app/doc/739db838-dd28-4078-8715-ee9cfcc06c29` : 'localhost:9176';
+const backendAdress = process.env.NODE_ENV === 'production' ? `${process.env.BACKEND}/app/doc/fc649d13-84ea-4174-8c96-67208f069587` : 'localhost:9176';
 
 const enigmaConfig = {
   schema: qixSchema,
